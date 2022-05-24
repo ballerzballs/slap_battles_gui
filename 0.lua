ad9e0017e0eaa454023d09c5c360f6e12845a429
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2022-05-24 03:09:15 +0300

    Update 0.lua

--- 0.lua
+++ 0.lua
@@ -3,4 +3,5 @@
     This script is in new repository: https://github.com/dizyhvh/rbx_scripts
 ]]
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dizyhvh/rbx_scripts/main/slap_battles.lua"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/dizyhvh/rbx_scripts/main/dizzy_hub/scripts/slap_battles.lua')))();
+
